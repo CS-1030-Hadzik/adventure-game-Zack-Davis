5d4d7cec154fe5423a2ead97ae3a68134e4bf3f8
Zack-Davis <zackdavis@mail.weber.edu>
2025-01-23 10:22:46 -0700

    Add input user

--- game.py
+++ game.py
@@ -1,2 +1,14 @@
-print("Welcome to the Adventure Game!")  
+"""
+Adventure Game
+Author: Zack Davis
+Version 1.0
+Description:
+This is a text-based adventure game where the player makes choices to navigate through a mysterious forest
+"""
+#Welcome message and introduction
+print("Welcome to the Adventure Game!")
 print("Your journey begins here...")
+# Ask for the players name
+Player_name = (input('What is your name adventurer?'))
+
+print('Welcome, ',  Player_name , '! Your journey begins now!')
